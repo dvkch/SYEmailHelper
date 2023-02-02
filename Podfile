ded5b53f3d65fddad672451972aad31430500d7a
DanBurkhardt <dan@gigabitelabs.com>
2023-02-02 01:45:14 -0500

    updated README with installation and example project instructions for both SPM and Cocoapods, removed cached deleted and moved files

--- Podfile
+++
@@ -1,9 +0,0 @@
-inhibit_all_warnings!
-
-platform :ios, '9.0'
-
-use_frameworks!
-
-target :'SYEmailHelperExample' do
-    pod 'SYEmailHelper', :path => "./"
-end
--- Pods/Target Support Files/Pods-SYEmailHelperExample/Pods-SYEmailHelperExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SYEmailHelperExample : NSObject
-@end
-@implementation PodsDummy_Pods_SYEmailHelperExample
-@end
